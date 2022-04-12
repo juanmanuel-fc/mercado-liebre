d0fb0051f2060864446eabb54be0453d1aef6539
juanmanuel-fc <juanmanuel.fc@gmail.com>
2022-04-12 15:37:14 -0300

    segundo commit - recomenzando

--- app.js
+++ app.js
@@ -1,13 +1,35 @@
-const express   = require("express");
-const app       = express();
-const path      = require("path");
+// const express   = require("express");
+// const app       = express();
+// const path      = require("path");
 
-app.listen(3030, () => {
-    console.log("Express corriendo OK");
-});
+// app.listen(3030, () => {
+//     console.log("Express corriendo OK");
+// });
 
+// const publicPath = path.resolve(__dirname, "./public");
+// app.use(express.static(publicPath));
+
+// app.get("/", (req, res) => {
+//     res.sendFile(path.resolve (__dirname, "./views/home.html"))
+// });
+
+// app.get("/home", (req, res) => {
+//     res.sendFile(path.resolve (__dirname, "./views/home.html"))
+// });
+
+const exp = require("constants");
+const express = require("express");
+const app = express();
+const puertoExpress = 3030;
+
+const path = require("path");
 const publicPath = path.resolve(__dirname, "./public");
 app.use(express.static(publicPath));
+
+
+app.listen(3030, () => {
+    console.log(`Express iniciado OK el el puerto ${puertoExpress}`);
+});
 
 app.get("/", (req, res) => {
     res.sendFile(path.resolve (__dirname, "./views/home.html"))
@@ -16,4 +38,3 @@ app.get("/", (req, res) => {
 app.get("/home", (req, res) => {
     res.sendFile(path.resolve (__dirname, "./views/home.html"))
 });
-
