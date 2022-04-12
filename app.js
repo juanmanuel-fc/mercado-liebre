b64eaa376b3c37441963d84dffc1815663d4f150
juanmanuel-fc <juanmanuel.fc@gmail.com>
2022-04-12 15:58:17 -0300

    primer micro desafio clase 1 OK

--- app.js
+++ app.js
@@ -1,23 +1,3 @@
-// const express   = require("express");
-// const app       = express();
-// const path      = require("path");
-
-// app.listen(3030, () => {
-//     console.log("Express corriendo OK");
-// });
-
-// const publicPath = path.resolve(__dirname, "./public");
-// app.use(express.static(publicPath));
-
-// app.get("/", (req, res) => {
-//     res.sendFile(path.resolve (__dirname, "./views/home.html"))
-// });
-
-// app.get("/home", (req, res) => {
-//     res.sendFile(path.resolve (__dirname, "./views/home.html"))
-// });
-
-const exp = require("constants");
 const express = require("express");
 const app = express();
 const puertoExpress = 3030;
