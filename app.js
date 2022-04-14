279087930abe3b74e1b7490a7b089dcd2456ec6a
juanmanuel-fc <juanmanuel.fc@gmail.com>
2022-04-14 06:15:23 -0300

    agregado la pagina de login y registro

--- app.js
+++ app.js
@@ -19,6 +19,14 @@ app.get("/home", (req, res) => {
     res.sendFile(path.resolve (__dirname, "./views/home.html"))
 });
 
+app.post("/home", (req, res) => {
+    res.sendFile(path.resolve (__dirname, "./views/home.html"))
+});
+
 app.get("/register", (req, res) => {
     res.sendFile(path.resolve (__dirname, "./views/register.html"))
+});
+
+app.get("/login", (req, res) => {
+    res.sendFile(path.resolve (__dirname, "./views/login.html"))
 });
