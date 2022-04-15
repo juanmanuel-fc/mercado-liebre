051c697d63c40f819c5afd41f54dc4075f370d54
juanmanuel-fc <juanmanuel.fc@gmail.com>
2022-04-15 12:32:53 -0300

    header responsive OK. Main responsive hasta mainArticle

---
+++ public/js/hamburger.js
@@ -0,0 +1,7 @@
+const hamburgerMenu = document.querySelector(".hamburger");
+
+const menuIsActive = () => {
+    hamburgerMenu.classList.toggle("active");
+};
+
+hamburgerMenu.addEventListener("click", menuIsActive);
